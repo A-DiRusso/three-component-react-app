db22511b1ff11b3d3388e58c03ccea24d105b0b9
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 15:39:15 -0400

    crates nav menu links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import SteveMartin from './SteveMartin';
 import ChevyChase from './ChevyChase';
 import MartinShort from './MartinShort';
-import { Route } from 'react-router-dom';
+import { 
+  Route,
+  Link
+ } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
@@ -17,6 +20,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Link to="/luckyday">Lucky Day</Link> 
+          <Link to="/dustybottoms">Dusty Bottoms</Link>
+          <Link to="/nednederlander">Ned Nederlander</Link>
           <Route path="/luckyday" component={SteveMartin} />
           <Route path="/dustybottoms" component={ChevyChase} />
           <Route path="/nednederlander" component={MartinShort} />
