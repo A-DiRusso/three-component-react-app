90986f2b26d51f40e5a330c0d2afd4ad8bba7966
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 16:10:56 -0400

    adds images

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+
 export default function Home() {
   return (
     <div>
-      <p>Welcome to the three components app:)</p> <br/>
+      <p>Welcome to the three components app:)</p>
       <p>Click a link to contiue</p>
+      <img alt='three' className="three-amigos" src={'./img/three.png'} />
     </div>
   )
 }
--- src/SteveMartin.js
+++ src/SteveMartin.js
@@ -4,6 +4,7 @@ export default function SteveMartin() {
   return (
     <div>
       <h1>Lucky Day</h1>
+      <img alt='lucky' className="lucky-day" src={'./img/LuckyDay..png'} />
     </div>
   )
 }
