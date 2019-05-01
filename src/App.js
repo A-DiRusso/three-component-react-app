a65e4d7030db6843f4850a3f3c8a72fec827cfb8
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 15:26:25 -0400

    makes App.js into a class

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import SteveMartin from './SteveMartin';
 import ChevyChase from './ChevyChase';
 import MartinShort from './MartinShort';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SteveMartin />
-        <ChevyChase />
-        <MartinShort />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SteveMartin />
+          <ChevyChase />
+          <MartinShort />
+        </header>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
