d5206957a497bac5a69b628d3d7a365df2e083db
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 15:43:02 -0400

    adds Home.js component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SteveMartin from './SteveMartin';
 import ChevyChase from './ChevyChase';
 import MartinShort from './MartinShort';
+import Home from './Home';
 import { 
   Route,
   Link
@@ -20,9 +21,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Link to="/">Home</Link>
           <Link to="/luckyday">Lucky Day</Link> 
           <Link to="/dustybottoms">Dusty Bottoms</Link>
           <Link to="/nednederlander">Ned Nederlander</Link>
+          <Route exact path="/" component={Home} />
           <Route path="/luckyday" component={SteveMartin} />
           <Route path="/dustybottoms" component={ChevyChase} />
           <Route path="/nednederlander" component={MartinShort} />
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <p>Welcome to the three components app:)</p> <br/>
+      <p>Click a link to contiue</p>
+    </div>
+  )
+}
