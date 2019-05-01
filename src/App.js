cd8f90d7c8425e6bde8220fb44316610e8c957cf
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 16:37:05 -0400

    adds 404 page not found routing and images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import SteveMartin from './SteveMartin';
 import ChevyChase from './ChevyChase';
 import MartinShort from './MartinShort';
 import Home from './Home';
+import PageNotFound from './PageNotFound';
 import { 
   Route,
-  Link
+  Link,
+  Switch
  } from 'react-router-dom';
 
 class App extends React.Component {
@@ -21,14 +23,17 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Link to="/">Home</Link>
-          <Link to="/luckyday">Lucky Day</Link> 
-          <Link to="/dustybottoms">Dusty Bottoms</Link>
-          <Link to="/nednederlander">Ned Nederlander</Link>
+          <Link activeClassName='active' to="/">Home</Link>
+          <Link activeClassName='active' to="/luckyday">Lucky Day</Link> 
+          <Link activeClassName='active' to="/dustybottoms">Dusty Bottoms</Link>
+          <Link activeClassName='active' to="/nednederlander">Ned Nederlander</Link>
+         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/luckyday" component={SteveMartin} />
           <Route path="/dustybottoms" component={ChevyChase} />
           <Route path="/nednederlander" component={MartinShort} />
+          <Route component={PageNotFound} />
+         </Switch>   
         </header>
       </div>
     );
